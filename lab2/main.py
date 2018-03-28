a67ba88b664524c89b42ebe1065c53e3d502c650
oHSUANo <chhu0830@gmail.com>
2018-03-28 23:02:35 +0800

    Revise option to use sys.argv

--- lab2/main.py
+++ lab2/main.py
@@ -8,11 +8,12 @@ training_images_file = (len(sys.argv) >= 2 and sys.argv[1]) or "data/train-image
 training_labels_file = (len(sys.argv) >= 3 and sys.argv[2]) or "data/train-labels-idx1-ubyte"
 testing_images_file  = (len(sys.argv) >= 4 and sys.argv[3]) or "data/t10k-images-idx3-ubyte"
 testing_labels_file  = (len(sys.argv) >= 5 and sys.argv[4]) or "data/t10k-labels-idx1-ubyte"
+option = (len(sys.argv) >= 6 and sys.argv[5]) or 0
 
 n, r, c, training_images = preprocess.images(training_images_file)
 n, training_labels = preprocess.labels(training_labels_file)
 
-naive_bayes = Naive(10, r * c, option=1)
+naive_bayes = Naive(10, r * c, option=option)
 naive_bayes.fit(training_images, training_labels)
 
 n, r, c, testing_images = preprocess.images(testing_images_file)
