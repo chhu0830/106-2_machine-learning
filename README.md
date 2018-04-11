872bf0adcf63202c87cfc068df00ce8fa0577c05
oHSUANo <chhu0830@gmail.com>
2018-04-12 01:42:01 +0800

    Revise some formula, plot and usage

--- README.md
+++ README.md
@@ -26,6 +26,6 @@ Assignments for machine-learning in NCTU
 - Usage: cd lab3 && python3 estimator.py &lt;number of data&gt; &lt;mean&gt; &lt;var&gt;
 
 ### Baysian Linear Regression
-- Usage: cd lab3 && python baysian.py &lt;number of data&gt; &lt;precision&gt; &lt;a&gt; &lt;w&gt;
+- Usage: cd lab3 && python baysian.py &lt;precision&gt; &lt;a&gt; &lt;w&gt;
   - a and w is for polynomial basis linear model data generator
 - Assume we have known the data variance
--- lab3/baysian.py
+++ lab3/baysian.py
@@ -5,46 +5,55 @@ from generator import Linear
 import sys
 
 
-N = int(sys.argv[1])
-b = float(sys.argv[2])
-a = float(sys.argv[3])
-w = [float(w) for w in sys.argv[4:]]
+b = float(sys.argv[1])
+a = float(sys.argv[2])
+w = [float(w) for w in sys.argv[3:]]
 n = len(w)
+
+px = []
+py = []
 
 linear = Linear(w, a)
 I = Matrix(n)
 
 s0 = I * b
 m0 = Matrix([[0 for i in range(n)]]).transpose()
-for _ in range(N):
+for _ in range(1000):
     x, y = linear.gen()
     X = Matrix([[x**i for i in range(n)]])
 
     s1 = X.transpose()*X*(1.0/a) + s0
-    m1 = s1.inverse() * (X.transpose()*a*y + s0*m0)
+    m1 = s1.inverse() * (X.transpose()*(1.0/a)*y + s0*m0)
 
     ym = (X*m1).data[0][0]
-    yv = (1.0/a) + (X*s1.inverse()*X.transpose()).data[0][0]
+    yv = a + (X*s1.inverse()*X.transpose()).data[0][0]
     print("x = %f, ym = %f, yv = %f, wm =" % (x, ym, yv), m1.transpose().data[0])
 
     s0, m0 = s1, m1
 
     if False:
         import matplotlib.pyplot as plt
-        l = Linear(m1.transpose().data[0], 0)
-        X = []
+        import numpy as np
+        import math
+        px.append(x)
+        py.append(y)
+        X = np.linspace(-10, 10, num=1000)
         Y = []
+        P = []
         U = []
         L = []
-        for i in range(10000):
-            x, y = l.gen()
-            X.append(x)
-            Y.append(y)
+        for x in X:
             d = Matrix([[x**i for i in range(n)]])
-            yv = (1.0/a) + (d*s1.inverse()*d.transpose()).data[0][0]
-            U.append(y+yv)
-            L.append(y-yv)
-        plt.scatter(X, U, c="red", s=1)
-        plt.scatter(X, L, c="red", s=1)
-        plt.scatter(X, Y, c="blue", s=1)
+            p = (d * m1).data[0][0]
+            v = (1.0/a) + (d*s1.inverse()*d.transpose()).data[0][0]
+            y = (d * Matrix([w]).transpose()).data[0][0]
+            Y.append(y)
+            P.append(p)
+            U.append(p+math.sqrt(v))
+            L.append(p-math.sqrt(v))
+        plt.plot(X, U, c="c")           # predict + deviation
+        plt.plot(X, L, c="c")           # predict - deviation
+        plt.plot(X, P, c="b")           # predict line
+        plt.plot(X, Y, c="r")           # true line
+        plt.scatter(px, py, c="g")
         plt.show()
